969a4066ffdc616045dcefd43bd9dc2e3d7ab0f3
WAN KHAIRUL AIMAN BIN WAN ALI <w.k.aiman1993@gmail.com>
2024-08-22 18:01:26 +0800

    files added

---
+++ project1.py
@@ -0,0 +1,36 @@
+# # 1. Use an integer as input and checks whether the number is even or odd.
+# number = 7
+
+# if number % 2 == 0:
+#     print("The number is even.")
+# else:
+#     print("The number is odd.")
+
+# # Output: The number is odd.
+
+# #2. multiplication table
+# number = int(input("Enter a number: "))
+# for i in range(1, 13):
+#     print(number, "x", i, "=", number * i)
+
+# Output: 
+
+#3.Takes three numbers as input from the user and prints the largest one.
+# x = int(input("Enter first number: "))
+# y = int(input("Enter second number: "))
+# z = int(input("Enter third number: "))
+
+# print ("the largest number of the series is: ", max(x,y,z)),
+
+# Input: 12,25,7   Output: 25
+ 
+# 4. Counts the number of vowels (a, e, i, o, u) in a given string.
+# text = input("Enter text: ")
+# count = 0
+# for char in text:
+#     if char in ("a", "e", "i", "o", "u"):
+#         count += 1
+# print("The number of vowels in", text, "is", count)
+    
+# Input: Hello World 
+# Output: The number of vowels in "Hello World" is 3.
