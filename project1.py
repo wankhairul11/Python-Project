50afd55212491885502d24795e58c9682650e175
WAN KHAIRUL AIMAN BIN WAN ALI <w.k.aiman1993@gmail.com>
2024-08-22 18:06:50 +0800

    updated file

--- project1.py
+++ project1.py
@@ -1,36 +1,38 @@
-# # 1. Use an integer as input and checks whether the number is even or odd.
-# number = 7
+# 1. Use an integer as input and checks whether the number is even or odd.
+number = 7
 
-# if number % 2 == 0:
-#     print("The number is even.")
-# else:
-#     print("The number is odd.")
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
 
-# # Output: The number is odd.
+# Output: The number is odd.
 
-# #2. multiplication table
-# number = int(input("Enter a number: "))
-# for i in range(1, 13):
-#     print(number, "x", i, "=", number * i)
+#2. multiplication table
+number = int(input("Enter a number: "))
+for i in range(1, 13):
+    print(number, "x", i, "=", number * i)
 
-# Output: 
+# Output: multiplication of numbers untill 
 
 #3.Takes three numbers as input from the user and prints the largest one.
-# x = int(input("Enter first number: "))
-# y = int(input("Enter second number: "))
-# z = int(input("Enter third number: "))
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+z = int(input("Enter third number: "))
 
-# print ("the largest number of the series is: ", max(x,y,z)),
+print ("the largest number of the series is: ", max(x,y,z)),
 
-# Input: 12,25,7   Output: 25
+# Input: 12,25,7    
+#  Output: the largest number of the series is:  25
+
  
-# 4. Counts the number of vowels (a, e, i, o, u) in a given string.
-# text = input("Enter text: ")
-# count = 0
-# for char in text:
-#     if char in ("a", "e", "i", "o", "u"):
-#         count += 1
-# print("The number of vowels in", text, "is", count)
+#4. Counts the number of vowels (a, e, i, o, u) in a given string.
+text = input("Enter text: ")
+count = 0
+for char in text:
+    if char in ("a", "e", "i", "o", "u"):
+        count += 1
+print("The number of vowels in", text, "is", count)
     
 # Input: Hello World 
 # Output: The number of vowels in "Hello World" is 3.
